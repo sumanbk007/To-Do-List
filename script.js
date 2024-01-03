ee9d2e24a807005f9674525dcccb075bd6799b22
Sumanbk007 <bksuman1211@gmail.com>
2024-01-03 16:50:44 +0545

    Edit feature added

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTask");
+const saveChangeButton = document.getElementById("saveChange");
 const taskList = document.getElementById("taskList");
+
+function createDeleteButton() {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("delete-button");
+  return deleteButton;
+}
+
+function createEditButton() {
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.classList.add("edit-button");
+  return editButton;
+}
 
 function addTask() {
   const taskText = taskInput.value.trim();
@@ -10,9 +25,13 @@ function addTask() {
     listItem.textContent = taskText;
 
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "X";
+    deleteButton.textContent = "Delete";
     deleteButton.classList.add("delete-button");
     listItem.appendChild(deleteButton);
+
+    const editButton = createEditButton();
+    console.log(editButton);
+    listItem.appendChild(editButton);
 
     taskList.appendChild(listItem);
 
@@ -24,8 +43,10 @@ function addTask() {
 
 function saveTaskToLocalStorage() {
   const tasks = Array.from(taskList.getElementsByTagName("li")).map(
-    (task) => task.textContent
+    (task, index) => task.childNodes[0].textContent.trim()
   );
+
+  // console.log(tasks);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
@@ -37,10 +58,11 @@ function loadTaskFromLocalStorage() {
 
     listItem.textContent = taskText;
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "X";
-    deleteButton.classList.add("delete-button");
+    const deleteButton = createDeleteButton();
+
     listItem.appendChild(deleteButton);
+    const editButton = createEditButton();
+    listItem.appendChild(editButton);
 
     taskList.appendChild(listItem);
   });
@@ -56,7 +78,33 @@ function deleteTask(event) {
   }
 }
 
+//Edit functionality
+
+function editTask(event) {
+  if (event.target.classList.contains("edit-button")) {
+    const listItem = event.target.parentElement;
+
+    taskInput.value = listItem.childNodes[0].textContent.trim();
+
+    // console.log(taskInput.value);
+
+    saveChangeButton.addEventListener("click", () => {
+      const taskText = taskInput.value.trim();
+      console.log(taskText);
+      listItem.textContent = taskText;
+      const deleteButton = createDeleteButton();
+      listItem.appendChild(deleteButton);
+      const editButton = createEditButton();
+      listItem.appendChild(editButton);
+      saveTaskToLocalStorage();
+
+      taskInput.value = "";
+    });
+  }
+}
+
 addTaskButton.addEventListener("click", addTask);
 taskList.addEventListener("click", deleteTask);
+taskList.addEventListener("click", editTask);
 
 window.addEventListener("load", loadTaskFromLocalStorage);
